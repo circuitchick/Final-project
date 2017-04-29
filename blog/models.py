0599be5fd2d2a56f1bceb2e86f1f2f0c4d6cbb05
Kareena Hudson <kareena.k.hudson@aggiemail.usu.edu>
2017-04-29 00:31:28 +0000

    trying to get posts to show

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ from django.utils import timezone
 # Create your models here.
 class BlogEntry(models.Model):
     title=models.CharField(max_length=100)
-    Body=models.TextField()
-    Date=models.DateTimeField()
+    body=models.TextField()
+    date=models.DateTimeField()
     
-def __str__(self):
-        return self.title
+    def __str__(self):
+     return self.title
