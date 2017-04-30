e5ce9d82f627a06b46f8bc2fea1ffa1edc1eab4f
Kareena Hudson <kareena.k.hudson@aggiemail.usu.edu>
2017-04-30 03:06:12 +0000

    finished blog code

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog.models import BlogEntry
 
 urlpatterns = [
     url(r'^$',ListView.as_view(queryset=BlogEntry.objects.all().order_by("-date")[:20],template_name="blog/blog.html")),
+    url(r'^(?P<pk>\d+)$',DetailView.as_view(model=BlogEntry, template_name="blog/blogentry.html")),
 ]
